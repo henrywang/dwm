1017e06149b7667046d798c76f1d527fc384a763
Xiaofeng Wang <hernywangxf@me.com>
2020-03-25 20:50:42 -0400

    Change killclient keybinding to mod+q instead of mod+shift+c Switch keybindings between open terminal and zoom

--- config.h
+++ config.h
@@ -96,7 +96,8 @@ static Key keys[] = {
 	{ 0,                       XF86XK_MonBrightnessDown,     spawn,          { .v = downbright } },
 
 	{ MODKEY,                       XK_p,      spawn,          { .v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          { .v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          { .v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           { 0 } },
 	{ MODKEY,                       XK_b,      togglebar,      { 0 } },
 	{ MODKEY,                       XK_j,      focusstack,     { .i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     { .i = -1 } },
@@ -104,9 +105,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     { .i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       { .f = -0.05 } },
 	{ MODKEY,                       XK_l,      setmfact,       { .f = +0.05 } },
-	{ MODKEY,                       XK_Return, zoom,           { 0 } },
 	{ MODKEY,                       XK_Tab,    view,           { 0 } },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     { 0 } },
 	{ MODKEY,                       XK_t,      setlayout,      { .v = &layouts[0] } },
 	{ MODKEY,                       XK_f,      setlayout,      { .v = &layouts[1] } },
 	{ MODKEY,                       XK_m,      setlayout,      { .v = &layouts[2] } },
@@ -127,6 +126,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY,                       XK_q,      killclient,     { 0 } },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           { 0 } },
 	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           { 1 } },
 };
